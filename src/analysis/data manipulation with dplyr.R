51279ee8c6ce96cd5b4080d508c4ea345896b925
jaclynjanis <jjanis@mmc.org>
2019-08-02 13:03:26 -0400

    tidy

---
+++ src/analysis/data manipulation with dplyr.R
@@ -0,0 +1,55 @@
+gapminder <- readr::read_csv(here("data/gapminder/raw/gapminder_data.csv"))
+
+head(gapminder)
+mean(gapminder$pop)
+mean(gapminder$gdpPercap[gapminder$continent == "Africa"])
+
+library(tidyverse)
+#Look at just year, country, GDP
+
+year_country_gdp <- select(gapminder, year, country, gdpPercap)
+head(year_country_gdp)
+
+#Just for Europe
+year_country_gdp_eur <- gapminder %>% 
+  filter(continent == "Europe") %>% 
+  select(year, country, gdpPercap)
+  
+head(year_country_gdp_eur)
+
+#Challenge 1
+afr_life_yr_country <- gapminder %>% 
+  filter(continent == "Africa") %>% 
+  select(lifeExp, year, country)
+
+
+grp_continent <- gapminder %>% 
+  group_by(continent)
+head(grp_continent)
+
+gapminder %>% 
+  group_by(continent) %>% 
+  summarize(mean_val = mean(gdpPercap))
+
+#Challenge 2
+
+gapminder %>% 
+  group_by(country) %>% 
+  summarize(mean_life = mean(lifeExp))
+gapminder %>% 
+  group_by(country) %>% 
+  summarize(mean_life = mean(lifeExp)) %>% 
+  filter(mean_life == min(mean_life) | mean_life == max(mean_life))
+
+gapminder %>% 
+  group_by(continent) %>% 
+  summarize(mean_val = mean(gdpPercap),
+            sd_gdpPercap = sd(gdpPercap))
+
+
+#facet_wrap for small multiples
+gapminder %>% 
+  filter(continent == "Africa") %>% 
+  ggplot(aes(x = year, y = lifeExp, color = continent)) +
+  geom_line() +
+  facet_wrap(~country)
--- src/gapminder/get gapminder data.R
+++ src/gapminder/get gapminder data.R
@@ -2,3 +2,7 @@
 library(here)
 file_url <- "https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv"
 
+
+#in terminal: mkdir data/gapminder/raw -p
+download.file(url = file_url, destfile = here("data/gapminder/raw/gapminder_data.csv"))
+
